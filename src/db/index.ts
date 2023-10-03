b24f0336dff8b70abca0547cafd0ee4f00f20e82
dev <dev@go-kay.com>
2023-10-03 22:23:08 +0900

    [Bugfix] pg module import editted

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,8 @@
-import { Pool } from 'pg';
+import pg from 'pg';
 import { DATABASE_POOL_URL } from '$env/static/private';
 import { drizzle } from 'drizzle-orm/node-postgres';
 
+const { Pool } = pg;
 const pool = new Pool({
 	connectionString: DATABASE_POOL_URL
 });
